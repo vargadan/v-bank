9dbec058a9dd8d40242e4a4d95fafa3e47de59a0
vargadan <danielvrg@gmail.com>
2019-03-28 22:11:49 +0100

    changes

--- attacker-app/src/main/java/com/hellokoding/springboot/view/AttackerApp.java
+++ attacker-app/src/main/java/com/hellokoding/springboot/view/AttackerApp.java
@@ -6,15 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class AttackerApp extends SpringBootServletInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
+        return application.sources(AttackerApp.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(AttackerApp.class, args);
     }
 
 }
